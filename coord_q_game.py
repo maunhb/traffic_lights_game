368acfefdfb24e00cbf0f256050014b4cb0e00c7
charlotte <lottie.d.roman@gmail.com>
2021-02-12 13:49:07 +0000

    coordinated q learning training

--- coord_q_game.py
+++ coord_q_game.py
@@ -3,31 +3,31 @@ import random
 import matplotlib.pyplot as plt 
 from game_dynamics.game_setup import GameVariables
 from game_dynamics.coord_agent import  CoordAgent 
+from game_dynamics.coord_env import  make_edge_list
 from game_dynamics.coord_epsilon_greedy import EpsilonGreedy
 from game_dynamics.coord_env import Environment
 from game_dynamics.variable_elimination import VariableElimination
 
+
+##  DEFINE COORDINATION GRAPH  
+## (dict) nodes and their neighbours
 coord_graph = {
     0:[1],
     1:[0]
 }
-# remove any duplicates from coord graph
-# coord edges represent the edges as a vector where 
-coord_edges = []
-vertex_list = list(coord_graph.keys())
-for vertex in coord_graph:
-    for i in range(0,len(coord_graph[vertex])):
-        if coord_graph[vertex][i] in vertex_list:
-            coord_edges = np.append(coord_edges, vertex)
-            coord_edges = np.append(coord_edges, coord_graph[vertex][i])
-    vertex_list.remove(vertex)
+coord_edges = make_edge_list(coord_graph)
+
+## CHOOSE ELIMINATION ORDERING
+## (list) node indexes
 elim_ordering = [0,1] 
 
-## simulate two individual traffic lights with q learning 
-num_p = 3
 
+num_p = 3 ## size of action space
 p = np.linspace(0.15,0.85,num_p)
 q = np.linspace(0.15,0.85,num_p)
+
+
+## SETUP ENVIRONMENT
 env = Environment(p=p, 
                   q=q, 
                   num_players_1=100, 
@@ -36,16 +36,20 @@ env = Environment(p=p,
 obs = env.reset()
 obs = np.around(obs, 1) 
 
+## SETUP COORD Q AGENTS 
 coord_agents = {edge: CoordAgent(joint_starting_state=[obs[int(coord_edges[edge])],
                                                        obs[int(coord_edges[edge+1])]],
-                            joint_state_space=[env.observation_space, env.observation_space],
-                            joint_action_space=[env.action_space,env.action_space],
-                            alpha=0.2,
-                            gamma=0.9) for edge in range(0,len(coord_edges),2)} 
-
+                                 joint_state_space=[env.observation_space, 
+                                                    env.observation_space],
+                                 joint_action_space=[env.action_space,
+                                                      env.action_space],
+                                 alpha=0.2,
+                                 gamma=0.9
+                                 ) for edge in range(0,len(coord_edges),2)} 
 
 strategy = EpsilonGreedy(initial_epsilon=0.8, min_epsilon=0.005, decay=0.99)
-env.reset()
+
+## RUN SIMULATION TO TRAIN AGENTS
 for time in range(env.game.sim_time):
 
     q_functions = {edge: coord_agents[edge].q_table['{}'.format(coord_agents[edge].state)] 
