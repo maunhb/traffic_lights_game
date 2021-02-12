190d8a1dffd96c71927323ea70d44bfb9ba0f210
charlotte <lottie.d.roman@gmail.com>
2021-02-12 13:55:01 +0000

    Multi-agent Q learning training

--- multi_q_game.py
+++ multi_q_game.py
@@ -6,23 +6,26 @@ from game_dynamics.multi_q import MultiQAgent
 from game_dynamics.multi_epsilon_greedy import EpsilonGreedy
 from game_dynamics.multi_env import Environment
 
-num_p = 3
-
+num_p = 3 ## size of action space
 p = np.linspace(0.15,0.85,num_p)
 q = np.linspace(0.15,0.85,num_p)
-env = Environment(p, q, 100, 100, 1500)#00)
+
+## SETUP ENVIRONMENT
+env = Environment(p, q, 100, 100, 150000)
+
 obs = env.reset()
 obs = np.around(obs, 1) 
 
+## SETUP MULTI-AGENT Q LEARNING AGENT
 ql_agents = {'TL': MultiQAgent(starting_state=obs,
                                action_space=[num_p, num_p],
-                               alpha=0.1,
-                               gamma=0.99,
+                               alpha=0.2,
+                               gamma=0.9,
                                exploration_strategy=EpsilonGreedy(initial_epsilon=0.8,
                                                                   min_epsilon=0.005, 
                                                                   decay=0.99))}
 
-env.reset()
+##  TRAINING 
 for time in range(env.game.sim_time):
 
     actions =  ql_agents['TL'].act() 
